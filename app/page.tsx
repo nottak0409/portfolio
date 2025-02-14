3bdd880a0fc417b7e6512e58e211a90475f5065d
nottak0409 <nottak0409@gmail.com>
2025-02-15 00:07:42 +0900

    メインページの修正

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-4 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<div className="text-xl sm:text-2xl font-bold text-center sm:text-left">
+				<div className="text-xl sm:text-2xl font-bold text-center sm:text-center">
 					ポートフォリオ
+				</div>
+				<div>
+					<Image
+						src="/stars.jpg"
+						width={500}
+						height={500}
+						alt="main image"
+					/>
 				</div>
 				<div className="flex gap-4 items-center flex-col sm:flex-row">
 					<a
 						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-						target="_blank"
+						href="/about"
 						rel="noopener noreferrer"
 					>
 						about me
@@ -25,16 +32,6 @@ export default function Home() {
 						resume
 					</a>
 				</div>
-				<ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-					<li className="mb-2">
-						Get started by editing{" "}
-						<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-							app/page.tsx
-						</code>
-						.
-					</li>
-					<li>Save and see your changes instantly.</li>
-				</ol>
 			</main>
 			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
 				<a
