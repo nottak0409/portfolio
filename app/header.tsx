f878f39a4336772c9c54160391c63852a45b7638
nottak0409 <nottak0409@gmail.com>
2025-02-15 00:37:37 +0900

    調整

---
+++ app/header.tsx
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Header() {
+	return (
+		<header className="row-start-1 flex gap-6 items-center justify-between bg-gray-800 w-full p-4 absolute top-0 z-10">
+			<Link href="/" className="flex items-center">
+				<Image src="/logo.png" alt="Logo" width={40} height={40} />
+			</Link>
+			<nav className="flex gap-6">
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href="/about"
+				>
+					<Image
+						aria-hidden
+						src="/globe.svg"
+						alt="Globe icon"
+						width={16}
+						height={16}
+					/>
+					About
+				</a>
+				<a
+					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+					href="/resume"
+				>
+					<Image
+						aria-hidden
+						src="/file.svg"
+						alt="File icon"
+						width={16}
+						height={16}
+					/>
+					Resume
+				</a>
+			</nav>
+		</header>
+	);
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,41 @@
 import Image from "next/image";
+import Header from "./header";
 
 export default function Home() {
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-4 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-				<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full">
-					ポートフォリオサイト
-				</div>
-				<div>
-					<Image
-						src="/stars.jpg"
-						width={500}
-						height={500}
-						alt="main image"
-					/>
-				</div>
-				<div className="flex gap-4 items-center justify-center flex-col sm:flex-row w-full">
-					<a
-						className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
-						href="/about"
-						rel="noopener noreferrer"
-					>
-						about me
-					</a>
-					<a
-						className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
-						href="/resume"
-						rel="noopener noreferrer"
-					>
-						resume
-					</a>
-				</div>
-			</main>
-			<footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/file.svg"
-						alt="File icon"
-						width={16}
-						height={16}
-					/>
-					Learn
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/window.svg"
-						alt="Window icon"
-						width={16}
-						height={16}
-					/>
-					Examples
-				</a>
-				<a
-					className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-					href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					<Image
-						aria-hidden
-						src="/globe.svg"
-						alt="Globe icon"
-						width={16}
-						height={16}
-					/>
-					Go to nextjs.org →
-				</a>
-			</footer>
+		<div>
+			<Header />
+			<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-4 font-[family-name:var(--font-geist-sans)]">
+				<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full">
+						ポートフォリオサイト
+					</div>
+					<div>
+						<Image
+							src="/stars.jpg"
+							width={500}
+							height={500}
+							alt="main image"
+						/>
+					</div>
+					<div className="flex gap-4 items-center justify-center flex-col sm:flex-row w-full">
+						<a
+							className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
+							href="/about"
+							rel="noopener noreferrer"
+						>
+							about me
+						</a>
+						<a
+							className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
+							href="/resume"
+							rel="noopener noreferrer"
+						>
+							resume
+						</a>
+					</div>
+				</main>
+			</div>
 		</div>
 	);
 }
