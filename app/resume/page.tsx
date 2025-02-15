5559a3a73c013ff621c6b99d912cc225eca2fdae
nottak0409 <nottak0409@gmail.com>
2025-02-16 08:24:51 +0900

    gapを修正

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -5,7 +5,7 @@ export default function About() {
 		<div>
 			<Header />
 			<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-				<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+				<main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
 					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full">
 						職務経歴
 					</div>
