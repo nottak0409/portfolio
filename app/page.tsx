631d2b796ff9b984a58d86df6c6e127af66b1448
nottak0409 <nottak0409@gmail.com>
2025-02-16 07:31:21 +0900

    星を背景に変更、ロゴをsvgに変更

--- app/page.tsx
+++ app/page.tsx
@@ -7,18 +7,11 @@ export default function Home() {
 			<Header />
 			<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-4 font-[family-name:var(--font-geist-sans)]">
 				<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full">
+					<Image src="/stars.jpg" alt="main image" fill />
+					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full z-10">
 						ポートフォリオサイト
 					</div>
-					<div>
-						<Image
-							src="/stars.jpg"
-							width={500}
-							height={500}
-							alt="main image"
-						/>
-					</div>
-					<div className="flex gap-4 items-center justify-center flex-col sm:flex-row w-full">
+					<div className="flex gap-4 items-center justify-center flex-col sm:flex-row w-full z-10">
 						<a
 							className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
 							href="/about"
