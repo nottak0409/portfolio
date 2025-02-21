75c8d16448dac34d618b57ca4c0c9761c67c81f5
nottak0409 <nottak0409@gmail.com>
2025-02-21 18:42:16 +0900

    アコーディオンメニューに変更

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -17,7 +17,7 @@ export default function About() {
 	return (
 		<div>
 			<Header />
-			<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
+			<div className="pt-28 min-h-screen p-8 gap-16 font-[family-name:var(--font-geist-sans)]">
 				<main className="flex flex-col gap-4 row-start-2 items-center z-10">
 					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full">
 						職務経歴
@@ -51,17 +51,25 @@ export default function About() {
 						>
 							フロントエンド ▼
 						</button>
-						<div className={isWindow("frontend") ? "" : "hidden"}>
-							<ul>
-								<li>HTML/CSS/JavaScript 4年</li>
-								<li>TypeScript 3年</li>
-								<li>React 3年</li>
-								<li>Redux 3年</li>
-								<li>Recoil 半年</li>
-								<li>Vue.js(2系) 1年</li>
-								<li>Next.js 業務外で学習</li>
-								<li>Node.js 2年</li>
-							</ul>
+						<div
+							className={`grid duration-500 ease-in-out ${
+								isWindow("frontend")
+									? "grid-rows-[1fr]"
+									: "grid-rows-[0fr]"
+							}`}
+						>
+							<div className="overflow-hidden">
+								<ul>
+									<li>HTML/CSS/JavaScript 4年</li>
+									<li>TypeScript 3年</li>
+									<li>React 3年</li>
+									<li>Redux 3年</li>
+									<li>Recoil 半年</li>
+									<li>Vue.js(2系) 1年</li>
+									<li>Next.js 業務外で学習</li>
+									<li>Node.js 2年</li>
+								</ul>
+							</div>
 						</div>
 						<button
 							className="text-lg font-bold"
@@ -69,19 +77,27 @@ export default function About() {
 						>
 							バックエンド ▼
 						</button>
-						<div className={isWindow("backend") ? "" : "hidden"}>
-							<ul>
-								<li>PHP 2年半</li>
-								<li>Ruby 2年</li>
-								<li>Laravel 2年</li>
-								<li>Ruby on Rails 2年</li>
-								<li>Go 業務外で学習</li>
-								<li>Rust 業務外で学習</li>
-								<li>express.js 業務外で学習</li>
-								<li>MySQL 4年</li>
-								<li>REST API 3年</li>
-								<li>GraphQL 業務外で学習</li>
-							</ul>
+						<div
+							className={`grid duration-500 ease-in-out ${
+								isWindow("backend")
+									? "grid-rows-[1fr]"
+									: "grid-rows-[0fr]"
+							}`}
+						>
+							<div className="overflow-hidden">
+								<ul>
+									<li>PHP 2年半</li>
+									<li>Ruby 2年</li>
+									<li>Laravel 2年</li>
+									<li>Ruby on Rails 2年</li>
+									<li>Go 業務外で学習</li>
+									<li>Rust 業務外で学習</li>
+									<li>express.js 業務外で学習</li>
+									<li>MySQL 4年</li>
+									<li>REST API 3年</li>
+									<li>GraphQL 業務外で学習</li>
+								</ul>
+							</div>
 						</div>
 						<button
 							className="text-lg font-bold"
@@ -89,11 +105,19 @@ export default function About() {
 						>
 							モバイル ▼
 						</button>
-						<div className={isWindow("mobile") ? "" : "hidden"}>
-							<ul>
-								<li>React Native 1年</li>
-								<li>Flutter 業務外で学習</li>
-							</ul>
+						<div
+							className={`grid duration-500 ease-in-out ${
+								isWindow("mobile")
+									? "grid-rows-[1fr]"
+									: "grid-rows-[0fr]"
+							}`}
+						>
+							<div className="overflow-hidden">
+								<ul>
+									<li>React Native 1年</li>
+									<li>Flutter 業務外で学習</li>
+								</ul>
+							</div>
 						</div>
 						<button
 							className="text-lg font-bold"
@@ -101,19 +125,27 @@ export default function About() {
 						>
 							その他 ▼
 						</button>
-						<div className={isWindow("other") ? "" : "hidden"}>
-							<ul>
-								<li>Git 4年半</li>
-								<li>GithubActions 2年</li>
-								<li>Terraform 業務内で少々</li>
-								<li>Serverless 業務内で少々</li>
-								<li>
-									AWS EC2/S3/RDS/Lambda/CloudWatch
-									業務内で少々
-								</li>
-								<li>Mackerel 業務内で少々</li>
-								<li>Docker 業務内で少々</li>
-							</ul>
+						<div
+							className={`grid duration-500 ease-in-out ${
+								isWindow("other")
+									? "grid-rows-[1fr]"
+									: "grid-rows-[0fr]"
+							}`}
+						>
+							<div className="overflow-hidden">
+								<ul>
+									<li>Git 4年半</li>
+									<li>GithubActions 2年</li>
+									<li>Terraform 業務内で少々</li>
+									<li>Serverless 業務内で少々</li>
+									<li>
+										AWS EC2/S3/RDS/Lambda/CloudWatch
+										業務内で少々
+									</li>
+									<li>Mackerel 業務内で少々</li>
+									<li>Docker 業務内で少々</li>
+								</ul>
+							</div>
 						</div>
 					</div>
 					<div className="text-xl sm:text-2xl font-bold text-center w-xl break-all mt-8">
