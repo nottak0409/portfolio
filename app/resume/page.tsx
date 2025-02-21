015b776eedb1f3194b6f99fe179e15f1be729c88
nottak0409 <nottak0409@gmail.com>
2025-02-21 18:17:23 +0900

    調整

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import { useState } from "react";
 import Header from "../header";
 
 export default function About() {
+	type windows = "frontend" | "backend" | "mobile" | "other";
+	const [openWindow, setOpenWindow] = useState<null | windows>(null);
+	const changeWindow = (window: windows): void => {
+		if (openWindow === window) {
+			setOpenWindow(null);
+			return;
+		}
+		setOpenWindow(window);
+	};
+	const isWindow = (window: windows): boolean => window === openWindow;
 	return (
 		<div>
 			<Header />
@@ -32,47 +45,76 @@ export default function About() {
 						スキル
 					</div>
 					<div className="text-center break-all flex flex-col gap-2">
-						<div className="text-lg font-bold">フロントエンド</div>
-						<ul>
-							<li>HTML/CSS/JavaScript 4年</li>
-							<li>TypeScript 3年</li>
-							<li>React 3年</li>
-							<li>Redux 3年</li>
-							<li>Recoil 半年</li>
-							<li>Vue.js(2系) 1年</li>
-							<li>Next.js 業務外で学習</li>
-							<li>Node.js 2年</li>
-						</ul>
-						<div className="text-lg font-bold">バックエンド</div>
-						<ul>
-							<li>PHP 2年半</li>
-							<li>Ruby 2年</li>
-							<li>Laravel 2年</li>
-							<li>Ruby on Rails 2年</li>
-							<li>Go 業務外で学習</li>
-							<li>Rust 業務外で学習</li>
-							<li>express.js 業務外で学習</li>
-							<li>MySQL 4年</li>
-							<li>REST API 3年</li>
-							<li>GraphQL 業務外で学習</li>
-						</ul>
-						<div className="text-lg font-bold">モバイル</div>
-						<ul>
-							<li>React Native 1年</li>
-							<li>Flutter 業務外で学習</li>
-						</ul>
-						<div className="text-lg font-bold">その他</div>
-						<ul>
-							<li>Git 4年半</li>
-							<li>GithubActions 2年</li>
-							<li>Terraform 業務内で少々</li>
-							<li>Serverless 業務内で少々</li>
-							<li>
-								AWS EC2/S3/RDS/Lambda/CloudWatch 業務内で少々
-							</li>
-							<li>Mackerel 業務内で少々</li>
-							<li>Docker 業務内で少々</li>
-						</ul>
+						<button
+							className="text-lg font-bold"
+							onClick={() => changeWindow("frontend")}
+						>
+							フロントエンド ▼
+						</button>
+						<div className={isWindow("frontend") ? "" : "hidden"}>
+							<ul>
+								<li>HTML/CSS/JavaScript 4年</li>
+								<li>TypeScript 3年</li>
+								<li>React 3年</li>
+								<li>Redux 3年</li>
+								<li>Recoil 半年</li>
+								<li>Vue.js(2系) 1年</li>
+								<li>Next.js 業務外で学習</li>
+								<li>Node.js 2年</li>
+							</ul>
+						</div>
+						<button
+							className="text-lg font-bold"
+							onClick={() => changeWindow("backend")}
+						>
+							バックエンド ▼
+						</button>
+						<div className={isWindow("backend") ? "" : "hidden"}>
+							<ul>
+								<li>PHP 2年半</li>
+								<li>Ruby 2年</li>
+								<li>Laravel 2年</li>
+								<li>Ruby on Rails 2年</li>
+								<li>Go 業務外で学習</li>
+								<li>Rust 業務外で学習</li>
+								<li>express.js 業務外で学習</li>
+								<li>MySQL 4年</li>
+								<li>REST API 3年</li>
+								<li>GraphQL 業務外で学習</li>
+							</ul>
+						</div>
+						<button
+							className="text-lg font-bold"
+							onClick={() => changeWindow("mobile")}
+						>
+							モバイル ▼
+						</button>
+						<div className={isWindow("mobile") ? "" : "hidden"}>
+							<ul>
+								<li>React Native 1年</li>
+								<li>Flutter 業務外で学習</li>
+							</ul>
+						</div>
+						<button
+							className="text-lg font-bold"
+							onClick={() => changeWindow("other")}
+						>
+							その他 ▼
+						</button>
+						<div className={isWindow("other") ? "" : "hidden"}>
+							<ul>
+								<li>Git 4年半</li>
+								<li>GithubActions 2年</li>
+								<li>Terraform 業務内で少々</li>
+								<li>Serverless 業務内で少々</li>
+								<li>
+									AWS EC2/S3/RDS/Lambda/CloudWatch
+									業務内で少々
+								</li>
+								<li>Mackerel 業務内で少々</li>
+								<li>Docker 業務内で少々</li>
+							</ul>
+						</div>
 					</div>
 					<div className="text-xl sm:text-2xl font-bold text-center w-xl break-all mt-8">
 						<a
