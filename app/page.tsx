c1b2960903ac44560d711c8dfe5f7e50b12a863b
nottak0409 <nottak0409@gmail.com>
2025-04-27 12:26:17 +0900

    topページのコンタクトのところ更新

--- app/page.tsx
+++ app/page.tsx
@@ -30,45 +30,102 @@ export default function Home() {
 							resume
 						</a>
 					</div>
-					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full z-10 mt-8">
+					<div className="text-xl sm:text-2xl font-bold text-center sm:text-center w-full z-10 mt-12">
 						contact me
-						<div className="text-base mt-8">
-							<div>
-								email:{" "}
-								<a href="mailto:rikuzen.noda0409@gmail.com">
-									rikuzen.noda0409@gmail.com
-								</a>
-							</div>
-							<div>
-								X:{" "}
-								<a
-									href="https://x.com/ntk673376240323"
-									rel="noopener noreferrer"
-									target="_blank"
+						<div className="flex gap-4 items-center justify-center flex-col sm:flex-row w-full flex-wrap mt-8">
+							<a
+								className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
+								href="mailto:rikuzen.noda0409@gmail.com"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 								>
-									ntk673376240323
-								</a>
-							</div>
-							<div>
-								Github:{" "}
-								<a
-									href="https://github.com/nottak0409"
-									rel="noopener noreferrer"
-									target="_blank"
+									<rect
+										width="20"
+										height="16"
+										x="2"
+										y="4"
+										rx="2"
+									/>
+									<path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
+								</svg>
+								Email
+							</a>
+							<a
+								className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
+								href="https://x.com/ntk673376240323"
+								rel="noopener noreferrer"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 								>
-									nottak0409
-								</a>
-							</div>
-							<div>
-								YouTrust:{" "}
-								<a
-									href="https://youtrust.jp/users/876e533eecfc1606bf495d380abae088"
-									rel="noopener noreferrer"
-									target="_blank"
+									<path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
+								</svg>
+								X
+							</a>
+							<a
+								className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
+								href="https://github.com/nottak0409"
+								rel="noopener noreferrer"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
 								>
-									YOUTRUST
-								</a>
-							</div>
+									<path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
+									<path d="M9 18c-4.51 2-5-2-7-2" />
+								</svg>
+								GitHub
+							</a>
+							<a
+								className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 min-w-44"
+								href="https://youtrust.jp/users/876e533eecfc1606bf495d380abae088"
+								rel="noopener noreferrer"
+								target="_blank"
+							>
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									width="20"
+									height="20"
+									viewBox="0 0 24 24"
+									fill="none"
+									stroke="currentColor"
+									strokeWidth="2"
+									strokeLinecap="round"
+									strokeLinejoin="round"
+								>
+									<path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+									<circle cx="9" cy="7" r="4" />
+									<path d="M22 21v-2a4 4 0 0 0-3-3.87" />
+									<path d="M16 3.13a4 4 0 0 1 0 7.75" />
+								</svg>
+								YouTrust
+							</a>
 						</div>
 					</div>
 				</main>
